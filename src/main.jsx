a54607ea3872001da3c312861f80036efa978b4e
Himansu Kumar Nayak <you@example.com>
2026-01-04 20:25:04 +0530

    Fix for vercel

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
+if (sessionStorage.redirect) {
+  history.replaceState(null, null, sessionStorage.redirect)
+  sessionStorage.removeItem('redirect')
+}
+
 createRoot(document.getElementById('root')).render(
+  
   <StrictMode>
     <BrowserRouter>
     <App />
